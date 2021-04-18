d25c1cc94f2b0a248aba3489a991871fd8ba6fc2
Nilesh D <nileshlund@gmail.com>
2021-04-18 20:11:10 +0530

    fix: Formatting Code using black

--- utilities.py
+++ utilities.py
@@ -1,16 +1,17 @@
 # Change the default width of bar in bar plot
 def change_width(plot, new_value):
-  for patch in plot.patches:
-    current_width = patch.get_width()
-    diff = current_width - new_value
+    for patch in plot.patches:
+        current_width = patch.get_width()
+        diff = current_width - new_value
 
-    # Change the bar width
-    patch.set_width(new_value)
+        # Change the bar width
+        patch.set_width(new_value)
 
-    # Recenter the bar
-    patch.set_x(patch.get_x() + diff * .5)
+        # Recenter the bar
+        patch.set_x(patch.get_x() + diff * 0.5)
+
 
 # Show values in each pie sector
 def show_values(pct, total):
-  absolute = round(pct / 100.*total)
-  return "{}".format(absolute)
+    absolute = round(pct / 100.0 * total)
+    return "{}".format(absolute)
